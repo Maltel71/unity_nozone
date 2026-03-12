cf3a8565a9f13a8fde6b9d86e5a9dc951d3b2def
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2026-03-12 10:37:33 +0100

    Malte scene paraflax

---
+++ Assets/Scrips/Enviroment_Scripts/ParallaxLayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [Range(0f, 1f)] public float parallaxAmount = 0.5f;
+    public bool affectX = true;
+    public bool affectY = false;
+
+    private Transform _cam;
+    private Vector3 _lastCamPos;
+
+    void Start()
+    {
+        _cam = Camera.main.transform;
+        _lastCamPos = _cam.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = _cam.position - _lastCamPos;
+
+        float moveX = affectX ? delta.x * (1f - parallaxAmount) : 0f;
+        float moveY = affectY ? delta.y * (1f - parallaxAmount) : 0f;
+
+        transform.position += new Vector3(moveX, moveY, 0f);
+        _lastCamPos = _cam.position;
+    }
+}
